1fc0d112d70d2233de3930e21c8469e5d2d3e419
Thiago Calura <thiagocalura@outlook.com>
2018-10-25 17:35:12 -0300

    CRIANDO UM OBJETO DATE A PARTIR DA ENTRADA DO USUÁRIO

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -13,10 +13,14 @@ class NegociacaoController {
   adiciona(event) {
     // cancelando a submissão do formulário
     event.preventDefault();
-    console.log(this._inputData.value);
-    console.log(parseInt(this._inputQuantidade.value));
-    console.log(parseFloat(this._inputValor.value));
-  
-  
+    let data = new Date(this._inputData.value.replace(/-/g, ','));
+    console.log(data);
+
+    let negociacao = new Negociacao(
+      data,
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    );
+    console.log(negociacao);
   }
 }
