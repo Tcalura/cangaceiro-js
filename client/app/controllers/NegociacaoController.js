b2b37fc139c183e0d7f16957b837aae612bee5c8
Thiago Calura <thiagocalura@outlook.com>
2018-10-25 19:01:13 -0300

    RESOLVENDO UM PROBLEMA COM O PARADIGMA FUNCIONAL

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,19 +1,26 @@
 class NegociacaoController {
+  
   constructor(){
     // a ideia é que $ seja o querySelector
     // realizando o bind, $ mantém document como seu contexto this
     let $ = document.querySelector.bind(document);
-
     // buscando os elementos
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-
   }
+
   adiciona(event) {
     // cancelando a submissão do formulário
     event.preventDefault();
-    let data = new Date(this._inputData.value.replace(/-/g, ','));
+    // spread operator ... faz com que cada posição do array se torne um parametro do construtor
+    let data = new Date(
+      ...this._inputData.value
+      .split('-')
+      .map(function(item, indice){
+        return item - indice % 2;
+      })
+    );
     console.log(data);
 
     let negociacao = new Negociacao(
