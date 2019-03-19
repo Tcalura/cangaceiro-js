7f38db7dc738f5e57a89093e1fe89c0e0f0d4ec6
Thiago Calura <thiago.rossi@connectcom.univesp.br>
2019-03-19 11:37:48 -0300

    Capitulo 10

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -8,35 +8,24 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-    const self = this;
-    this._negociacoes = new Proxy(new Negociacoes(), {
-      get(target, prop, receiver) {
-        if(typeof(target[prop]) == typeof(Function) 
-          && ['adiciona', 'esvazia'].includes(prop)) {
-            return function() {
-              console.log(`"${prop}" disparou a armadilha`);
-              target[prop].apply(target, arguments);
-              self._negociacoesView.update(target);
-            }
-        } else {
-          // realizando um get padrão
-          return target[prop];
-        }
-      }
-    });
-    this._negociacoesView = new NegociacoesView('#negociacoes');
-    // atualizando a view
-    this._negociacoesView.update(this._negociacoes);
-    this._mensagem = new Mensagem();
-    this._mensagemView = new MensagemView('#mensagemView');
-    this._mensagemView.update(this._mensagem);
+
+    this._negociacoes = new Bind( 
+      new Negociacoes,
+      new NegociacoesView('#negociacoes'),
+      'adiciona', 'esvazia'
+    );
+
+    this._mensagem = new Bind(
+      new Mensagem,
+      new MensagemView('#mensagemView'),
+      'texto'      
+    );
   }
 
   adiciona(event) {
     event.preventDefault();
     this._negociacoes.adiciona(this._criaNegociacao());
     this._mensagem.texto = 'Negociação adicionada com sucesso';
-    this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
   }
 
@@ -59,7 +48,6 @@ class NegociacaoController {
   apaga() {
     this._negociacoes.esvazia();
     this._mensagem.texto = 'Negociações apagadas com sucesso';
-    this._mensagemView.update(this._mensagem);
   }
 
 }
